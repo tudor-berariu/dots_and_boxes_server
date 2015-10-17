48a194bb0ee976e6286aff4d1cea6dd988f4545b
Tudor Berariu <tudor.berariu@gmail.com>
2015-10-17 22:36:59 +0300

    Added a slightly better player

--- src/game_server.py
+++ src/game_server.py
@@ -1,7 +1,7 @@
 # Tudor Berariu, 2015
 
 from random import choice
-from itertools import count
+from itertools import count, izip, product
 from signal import signal, alarm, SIGALRM
 
 def get_move(board, score, move_fnc):
@@ -25,23 +25,46 @@ def play_game(player0, player1, height=10, width=10):
     try:
       (r, c) = get_move(board, (score[current], score[1-current]), players[current].move)
       if board[r][c] != 0:
+        # This means your algorithm sucks!
         return (cells_no * current, cells_no * (1-current), "wrong move")
       else:
         board[r][c] = 1
         if r % 2 == 0:
+          # Check if cell above is closed now
           if r > 0 and sum([board[x][y] for (x,y) in [(r-2,c), (r-1,c), (r-1,c+1)]]) == 3:
             score = (score[0] + (1-current), score[1] + current)
+          # Check if cell below is closed now
           if r < 2 * height and sum([board[x][y] for (x,y) in [(r+2,c), (r+1,c), (r+1,c+1)]]) == 3:
             score = (score[0] + (1-current), score[1] + current)
         else:
+          # Check if cell on left is closed now
           if c > 0 and sum([board[x][y] for (x,y) in [(r-1,c-1), (r, c-1), (r+1, c-1)]]) == 3:
             score = (score[0] + (1-current), score[1] + current)
+          # Check if cell on right is closed now
           if c < width and sum([board[x][y] for (x,y) in [(r-1,c), (r, c+1), (r+1, c)]]) == 3:
             score = (score[0] + (1-current), score[1] + current)
     except Exception as e:
       return (cells_no * current, cells_no * (1-current), str(e))
     current = 1 -current
   return (score[0], score[1], "ok")
+
+class BetterPlayer:
+  def __init__(self):
+    pass
+
+  def move(self, board, score):
+    cells_height = (len(board) - 1) / 2
+    cells_width = len(board[0])
+    # Look for a cell with 3 walls
+    for r, c in product(range(cells_height), range(cells_width)):
+        cells = [(r*2,c), (r*2+2,c), (r*2+1,c), (r*2+1,c+1)]
+        if sum(map(lambda (x, y): board[x][y], cells)) == 3:
+            return next((x,y) for (x,y) in cells if board[x][y] == 0)
+    # If there was no such cell, pick a random one
+    good_rows = filter(lambda i: 0 in board[i], range(len(board)))
+    row = choice(good_rows)
+    col = choice([c for c, val in izip(count(), board[row]) if val == 0])
+    return row, col
 
 class RandomPlayer:
   def __init__(self):
@@ -66,5 +89,5 @@ class WrongPlayer:
 
 if __name__ == "__main__":
   p1 = RandomPlayer()
-  p2 = WrongPlayer()
+  p2 = BetterPlayer()
   print(play_game(p2, p1))
